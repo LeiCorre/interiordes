d61db6920315bf95b1208ef9375d7fa8e99d86f5
LeiCorre <jen.y.loaa@gmail.com>
2021-04-26 14:04:44 -0600

    .

--- docs/colScript.js
+++ docs/colScript.js
@@ -1,7 +1,7 @@
-function viewPic () {
+ function viewPic () {
     let view = document.getElementsByClassName('view')[0]
     let x = event.target.getAttribute('src')
     let xwidth = event.target.clientWidth;
     let xheight = event.target.clientHeight;
     view.setAttribute('style', 'background-image: url("' + x + '"); background-repeat: no-repeat;') 
-}
+} 
